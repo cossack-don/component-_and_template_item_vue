26a91ece07ebd7dfd1fc237029f381f5d8fd7b45
Sergei <ser90593696@yandex.ru>
2021-03-07 15:58:17 +0300

    add template comp+item_template

---
+++ registr-redirect/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+<!-- Компонент переиспользуемый ITEM -->
+
+<!-- баендим данные :product = item . Получаем данные в данном компоненте APP => array with obj 
+чтобы отрисовать все данные из обьекта который получили циклом for , мы задаем product = item
+
+ -->
+    <imp v-for="(item,index) in items" :key="index"
+    :product="item"
+    />
+<!-- END Компонент переиспользуемый ITEM -->
+  </div>
+</template>
+
+
+
+<script>
+import imp from '@/components/item'
+
+export default {
+components:{
+  imp
+},
+data() {
+  return {
+    // array data
+    items:[
+      {
+        title:'title-1',
+        discr:'discr------2'
+      },
+            {
+        title:'title-2',
+        discr:'discr------233'
+      },
+            {
+        title:'title-3',
+        discr:'discr------2333'
+      }
+    ]
+  }
+},
+
+}
+</script>
---
+++ registr-redirect/src/components/item.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="background:yellow; margin:10px; padding:15px;">
+      <p style="color:green;">paragraph - 1 {{product.title}}</p>
+      <p>paragraph - 2 {{product.discr}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+// пропсы обычно передаются в дочернем компоненте для принятия их в родительском компоненте
+
+
+    // передаем пропсы App => item компонент, чтобы отрисовать данные
+    // нужно чтобы тип был обьектом,обязательно
+    // product это название которые мы баендим в app компоненте, можно назвать произвольно
+props: {
+    product: {
+        type: Object,
+        required:true
+    }
+}
+}
+</script>
